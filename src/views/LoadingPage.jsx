fd33f21bc27a756e8e075976f6012f2786552198
Mekmun Sopheaktra <Mekmunsopheaktra@gmail.com>
2023-02-04 12:59:03 +0700

    forum

--- src/views/LoadingPage.jsx
+++ src/views/LoadingPage.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import spinner from '../assets/icons/Spinner.svg'
 const StyledWrapper = styled.div`
   background-color: hsl(228, 56%, 98%);
   font-size: 40px;
